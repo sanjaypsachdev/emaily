bab3ded42d2e23c7987c1089b42232dae183a02c
ssachdev <sanjay.sachdev@hotmail.com>
2020-12-04 12:30:34 +0530

    Added production deployment spicific code for heroku

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // If Express does not find any route defined then
+  // it will look for the specified file path inside client/build.
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file if it does not recognize the route.
+  // Catch all case !
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
